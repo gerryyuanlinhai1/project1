43d3d128782734df374ad2788a3bc240b69e1453
linhai <linhai@atguigu.com>
2020-09-01 18:08:30 +0800

    提交Test

--- comttt/src/main/java/com/atguigu/redis/test/Test.java
+++ comttt/src/main/java/com/atguigu/redis/test/Test.java
@@ -7,7 +7,7 @@ package com.atguigu.redis.test;
 public class Test {
     public static void main(String[] args) {
         System.out.println("hello world");
-        System.out.println("hello world");
+        System.out.println("hello world");//master edity
 
         System.out.println("hello world");
         System.out.println("hello world");
