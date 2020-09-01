b06301169b80d796f9f3a84726687b5f82cedf04
linhai <linhai@atguigu.com>
2020-09-01 18:09:07 +0800

    提交Test hotfix

--- comttt/src/main/java/com/atguigu/redis/test/Test.java
+++ comttt/src/main/java/com/atguigu/redis/test/Test.java
@@ -7,7 +7,7 @@ package com.atguigu.redis.test;
 public class Test {
     public static void main(String[] args) {
         System.out.println("hello world");
-        System.out.println("hello world");
+        System.out.println("hello world");//edit hotfix
 
         System.out.println("hello world");
         System.out.println("hello world");
